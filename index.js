e77f9424bc6a21f38686bf67d2962480e986bbe1
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2024-02-25 16:24:33 +0100

    feat(index.js): :sparkles: log file size of the current and previous file on file change

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const fs = require("fs");
 
-fs.watchFile("./index.html", () => console.log("listener called"));
+fs.watchFile("./index.html", (current, previous) => {
+  console.log(`File size of the current file: ${current.size} bytes`);
+  console.log(`File size of the previous file: ${previous.size} bytes`);
+});
