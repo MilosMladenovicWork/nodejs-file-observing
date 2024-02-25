daba5dcf40542806f2c099548c9d6a09f8eb58ba
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2024-02-25 16:41:26 +0100

    feat(index.js): :sparkles: use chalk to color console logs

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const fs = require("fs");
-const dns = require("node:dns");
+import chalk from "chalk";
+import fs from "fs";
+import dns from "node:dns";
 
 fs.watchFile("./index.html", (current, previous) => {
-  console.log(`File size of the current file: ${current.size} bytes`);
-  console.log(`File size of the previous file: ${previous.size} bytes`);
+  console.log(
+    chalk.orange(`File size of the current file: ${current.size} bytes`)
+  );
+  console.log(
+    chalk.orange(`File size of the previous file: ${previous.size} bytes`)
+  );
 });
 
 dns.lookup("google.com", (err, address) => {
   if (err) {
-    return console.log(err);
+    return console.log(chalk.red(err));
   }
 
-  console.log(address);
+  console.log(chalk.blue(address));
 });
