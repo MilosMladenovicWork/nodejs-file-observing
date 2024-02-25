b281c7f95866c93b2da7b55de145ed8221a928af
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2024-02-25 16:35:38 +0100

    feat(index.js): :sparkles: use dns lookup

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const fs = require("fs");
+const dns = require("node:dns");
 
 fs.watchFile("./index.html", (current, previous) => {
   console.log(`File size of the current file: ${current.size} bytes`);
   console.log(`File size of the previous file: ${previous.size} bytes`);
+});
+
+dns.lookup("google.com", (err, address) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log(address);
 });
